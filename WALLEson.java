523e102be5fd5869cd0d484bc06b1674e3dbf33f
Arthur <arthur.antonelli76@gmail.com>
2025-10-14 22:14:25 -0300

    Comando para o robo circular as paredes e atirar nos inimigos

--- WALLEson.java
+++ WALLEson.java
@@ -13,20 +13,18 @@ public class WALLEson extends Robot
 	 * run: WALLEson's default behavior
 	 */
 	public void run() {
-		// Initialization of the robot should be put here
+		turnRight(180 - getHeading());
+		if (getY() > getBattleFieldHeight()){
+			ahead(getBattleFieldHeight() - getY());
+		}else{
+			ahead(getY() - getBattleFieldHeight());
+		}
+		turnGunLeft(90);
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			ahead(-1000);
+			
 		}
 	}
 
@@ -36,21 +34,19 @@ public class WALLEson extends Robot
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
 		fire(1);
+		
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
+		
 		// Replace the next line with any behavior you would like
-		back(10);
 	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
+
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(20);
+		turnRight(90);
 	}	
 }
