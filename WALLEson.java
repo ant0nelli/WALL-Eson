b229060094f26316a9279605a52126a18a09ce95
Arthur <arthur.antonelli76@gmail.com>
2025-10-27 18:07:05 +0000

    melhoria para andar pelas paredes

--- WALLEson.java
+++ WALLEson.java
@@ -1,4 +1,4 @@
-package ifsc;
+package ifsc.WALLEson;
 import robocode.*;
 //import java.awt.Color;
 
@@ -13,17 +13,45 @@ public class WALLEson extends Robot
 	 * run: WALLEson's default behavior
 	 */
 	public void run() {
-		turnRight(180 - getHeading());
-		if (getY() > getBattleFieldHeight()){
-			ahead(getBattleFieldHeight() - getY());
-		}else{
-			ahead(getY() - getBattleFieldHeight());
-		}
-		turnGunLeft(90);
+        double largura = getBattleFieldWidth();
+        double altura = getBattleFieldHeight();
+        double meioX = largura / 2;
+        double meioY = altura / 2;
+
+       
+        double x = getX();
+        double y = getY();
+
+        // Virar o robô "para cima" (0° é o norte)
+        turnRight(360 - getHeading());
+
+        // Ir até a parede mais próxima
+        if (y < meioY && y < (altura - y) && y < x && y < (largura - x)) {
+            // Está mais perto da parede de baixo
+			turnRight(180 - getHeading());
+			ahead(getY() - 20);
+            
+        } else if ((altura - y) < meioY && (altura - y) < x && (altura - y) < (largura - x)) {
+            // Mais perto da parede de cima
+            turnRight(0 - getHeading());
+            ahead(altura - getY() - 20);
+
+        } else if (x < meioX && x < (largura - x)) {
+            // Mais perto da parede da esquerda
+            turnRight(270 - getHeading());
+            ahead(getX() - 20);
+
+        } else {
+            // Mais perto da parede da direita
+            turnRight(90 - getHeading());
+            ahead(largura - getX() - 20);
+        }
+		turnGunRight(90);
+
 
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			ahead(-1000);
+			ahead(1000);
 			turnGunRight(90);
 			turnGunLeft(180);
 			turnGunRight(90);
